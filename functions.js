b3558492f3d86784d8737ffdbd19949c85e874e6
Rogério Brito <rbrito@ime.usp.br>
2012-01-21 11:20:36 -0200

    functions: Use hints from JSLint for Javascript.

--- functions.js
+++ functions.js
@@ -1,52 +1,54 @@
 function translate_status(status) {
+    "use strict";
     return status ? "yes" : "no";
 }
 
+
 function checkVideo() {
+    "use strict";
+    var v_test, ogg_playable, webm_playable, h264_playable, msg_preamble, msg_postamble, msg_sorry;
+
+    msg_sorry = "<p>Sorry. No video support at all.</p>" + msg_postamble;
+    msg_preamble = "<p>Here is your video support result: <ul> ";
+    msg_postamble = "<input type='button' value='Reload Page' onclick='window.location.reload()'>";
+
     if (!!document.createElement('video').canPlayType) {
-	var v_test, ogg_playable, webm_playable, h264_playable;
-	var msg_preamble;
-	var msg_postamble;
+        v_test = document.createElement("video");
 
-	v_test = document.createElement("video");
+        ogg_playable = v_test.canPlayType('video/ogg; codecs="theora, vorbis"');
+        h264_playable = v_test.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
+        webm_playable = v_test.canPlayType('video/webm; codecs="vp8, vorbis"');
 
-	ogg_playable = v_test.canPlayType('video/ogg; codecs="theora, vorbis"');
-	h264_playable = v_test.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
-	webm_playable = v_test.canPlayType('video/webm; codecs="vp8, vorbis"');
-
-	msg_preamble = "<p>Here is your video support result: <ul> ";
-	msg_postamble = "<input type='button' value='Reload Page' onclick='window.location.reload()'>";
-
-	document.getElementById("checkVideoResult").innerHTML =
-	    msg_preamble +
-	    "<li>Ogg (Theora/Vorbis)= " + translate_status(ogg_playable) + ".</li> " +
-	    "<li>H.264 (AVC/AAC)= " + translate_status(h264_playable) + ".</li> " +
-	    "<li>WebM (VP8/Vorbis)= " + translate_status(webm_playable) + ".</li> " +
-	    "</ul> </p>" +
-	    msg_postamble;
+        document.getElementById("checkVideoResult").innerHTML =
+            msg_preamble +
+            "<li>Ogg (Theora/Vorbis)= " + translate_status(ogg_playable) + ".</li> " +
+            "<li>H.264 (AVC/AAC)= " + translate_status(h264_playable) + ".</li> " +
+            "<li>WebM (VP8/Vorbis)= " + translate_status(webm_playable) + ".</li> " +
+            "</ul> </p>" +
+            msg_postamble;
     } else {
-	var msg_sorry = "<p>Sorry. No video support at all.</p>" + msg_postamble;
-	document.getElementById("checkVideoResult").innerHTML = msg_sorry;
+        document.getElementById("checkVideoResult").innerHTML = msg_sorry;
     }
 }
 
 
 function getLocation() {
-    var x = document.getElementById("geoloc");
-    var msg_postamble = "<input type='button' value='Reload Page' onclick='window.location.reload()'>";
+    "use strict";
+    var x = document.getElementById("geoloc"),
+        msg_postamble = "<input type='button' value='Reload Page' onclick='window.location.reload()'>";
 
     function showPosition(position) {
-	x.innerHTML =
-	    "<p>Latitude: " +
-	    position.coords.latitude +
-	    "<br />Longitude: " + position.coords.longitude + "</p>" +
-	    msg_postamble;
+        x.innerHTML =
+            "<p>Latitude: " +
+            position.coords.latitude +
+            "<br />Longitude: " + position.coords.longitude + "</p>" +
+            msg_postamble;
     }
 
     if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-	x.innerHTML = "<p>Geolocation is not supported by this browser.</p>" +
+        x.innerHTML = "<p>Geolocation is not supported by this browser.</p>" +
             msg_postamble;
     }
 }
